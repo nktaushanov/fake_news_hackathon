219fb75419aaa4cf7dc32d835a1172ba69969335
Vassil Lunchev <vassil@leanplum.com>
2017-05-20 16:43:51 +0300

    test on evaluation set

--- classifier.py
+++ classifier.py
@@ -18,6 +18,9 @@ def vectorize_column(df_column, vectorizer, tokenize):
     all_articles = [tokenize(value) for value in list(df_column)]
     return vectorizer.fit_transform(all_articles)
 
+def preprocess_text(df_column, tokenize):
+    return [tokenize(value) for value in list(df_column)]
+
 
 def content_dicision_tree(df):
     articles_vectors = vectorize_column(df['Content'],
@@ -50,13 +53,16 @@ def domain_dicision_tree(df):
 
 
 def combined_classifier(df):
-    articles_vectors = vectorize_column(df['Content'],
-            text.CountVectorizer(analyzer=lambda x: x),
-            lambda article: tokenizer.article2words(article, flatten=True))
+    article_fit_vector = text.CountVectorizer(analyzer=lambda x: x)
+    domain_fit_fector = text.CountVectorizer(analyzer=lambda x: x)
 
-    domain_vectors = vectorize_column(df['Content Url'],
-            text.CountVectorizer(analyzer=lambda x: x),
+    article_preprocessed = preprocess_text(df['Content'],
+            lambda article: tokenizer.article2words(article, flatten=True))
+    domain_preprocessed = preprocess_text(df['Content Url'],
             lambda url: utils.get_domain(url))
+
+    articles_vectors = article_fit_vector.fit_transform(article_preprocessed)
+    domain_vectors = domain_fit_fector.fit_transform(domain_preprocessed)
 
     combined_vectors = sparse.hstack((articles_vectors, domain_vectors))
 
@@ -68,6 +74,23 @@ def combined_classifier(df):
     print classifier.score(combined_vectors, scores)
 
 
+    eval_df = corpora.train_set('FN_Evaluation_Set.csv')
+    eval_article_preprocessed = preprocess_text(eval_df['Content'],
+            lambda article: tokenizer.article2words(article, flatten=True))
+    eval_domain_preprocessed = preprocess_text(eval_df['Content Url'],
+            lambda url: utils.get_domain(url))
+
+    eval_articles_vectors = article_fit_vector.transform(eval_article_preprocessed)
+    eval_domain_vectors = domain_fit_fector.transform(eval_domain_preprocessed)
+    eval_combined_vectors = sparse.hstack((eval_articles_vectors, eval_domain_vectors))
+    eval_scores = list(eval_df['fake_news_score'])
+    
+    print classifier.score(eval_combined_vectors, eval_scores)
+
+
+
+
+
 if __name__ == "__main__":
-    df = corpora.train_set()
+    df = corpora.train_set('FN_Training_Set.csv')
     combined_classifier(df)
--- corpora.py
+++ corpora.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import utils
 
-def train_set():
-    df = pd.read_csv(utils.get_project_file_path('data', 'FN_Training_Set.csv'), encoding='utf8')
+def train_set(filename):
+    df = pd.read_csv(utils.get_project_file_path('data', filename), encoding='utf8')
     df['is_basestring'] = df['Content'].apply(lambda c: isinstance(c, basestring))
     df = df[df['is_basestring'] == True]
     return df.drop('is_basestring', axis=1)
